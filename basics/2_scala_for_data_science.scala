082532d8e80ad5606e4d7f2b42245d84f930e134
sriram161 <k.sriramsagar@gmail.com>
2019-04-21 17:24:06 -0400

    variables and expressions Scala.

--- basics/2_scala_for_data_science.scala
+++ basics/2_scala_for_data_science.scala
@@ -1,35 +1,28 @@
 /**
-* This file discusses Scalar data tyes and Array data types in Scala.
+* This file discusses maps.
 */
-// Scalar data types
-var a_int : Int = 3
-var a_char : Char = 'd'
-var a_long : Long = 8345679
-var b = 3
-var b_char = 'b'
-var b_long = 83456778  // gives error scala tries to push data to int.
-var b_long = 83444545456L 
-var b_float = 834.44545456f
 
-// collections [Mutable or Immutable]
-val temps=Array(50, 51, 40)
-// 0 index Array
-temps(0)
-temps.length
-temps(0) = 54
-val temps2: Array[Int] = new Array[Int](10)
+val capitals = Maps("Argentian" <- "Buenos Aires",
+"Canada" -> "Ottowa",
+"United States" -> "Washington.D.C")
 
-val temp3 = Array.ofDim[Int](10, 10)
+capitals. // Press tab
 
-import Array._
+capitals.keys
 
-concat(temps, temps2)
+capitals.values
 
-temps. // press tab. to find all the methods that can be run on a variable.
+capitals get 'Canada'
 
-val vec1: Vector[Int] = Vector(1,2,3,4,5,6,7,8,9,0)
-vec1(2)
+capitals get 'India'
 
-val myRange = 1 t0 10
-val MyRange2: Range = new Range(1, 10, 2)
+capitals('Canada')
+
+capitals contains "Egypt"
+
+capitals getOrElse("China", "No Capitals found")
+
+capitals + ("Ireland"-> "Dublin")
+
+capitals - "Canada"
 
---
+++ basics/3_scala_for_data_science.scala
@@ -0,0 +1,32 @@
+/**
+* This file discusses expressions.
+*/
+
+2 + 2
+100 - 2
+4 * 56
+33 / 4
+33 % 3
+
+
+3>4
+5<10
+
+3>4 && 5<=10
+3>4 || 5<=10
+! (3>4)
+
+var a  = 10
+var b  = 20
+var c = 30
+
+// short hand expression.
+c += a
+c *= a
+c 
+
+// Blocking is done using {}
+println({
+    val a= 2*3
+    a + 4
+})
